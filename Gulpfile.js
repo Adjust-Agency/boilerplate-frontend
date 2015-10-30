f247b7e28756148c64cc24e4ffa43faaa4c37bf5
Lemagus <gabriel@adjust.be>
2015-10-30 11:06:03 +0100

    First draft gulp boilerplate

---
+++ Gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass'),
+	minifyCSS = require('gulp-minify-css'),
+	sourcemaps = require('gulp-sourcemaps'),
+	autoprefixer = require('gulp-autoprefixer');
+var minifyJS = require('gulp-minify');
+var uglify = require('gulp-uglify'),
+	jshint = require('gulp-jshint'),
+	stylish = require('jshint-stylish');
+var imagemin = require('gulp-imagemin');
+var connect = require('gulp-connect');
+
+var src  = "./src/";
+var dist = "./dist/";
+
+gulp.task('connect', function() {
+  connect.server({
+    port: 3000
+  });
+});
+
+gulp.task('styles', function() {
+    gulp.src( src + 'sass/**/*.scss')
+    	.pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(minifyCSS())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest( dist + 'css/'));
+});
+
+gulp.task('javascript', function() {
+  	gulp.src([ src + 'js/**/*.js'])
+  		.pipe(jshint())
+  		.pipe(jshint.reporter(stylish))
+  		//.pipe(jshint.reporter('fail'))
+  		.pipe(sourcemaps.init())
+  		.pipe(concat('main.js'))
+  		.pipe(uglify())
+  		.pipe(sourcemaps.write('./'))
+  		.pipe(gulp.dest( dist + 'js/'));
+});
+
+// gulp.task('images', function () {
+//     return gulp.src( src + 'images/**/*')
+//         .pipe(imagemin({
+//             progressive: true,
+//             svgoPlugins: [{removeViewBox: false}]
+//         }))
+//         .pipe(gulp.dest(dist + 'images/'));
+// });
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch( src + 'sass/**/*.scss',['styles']);
+    gulp.watch( src + 'js/**/*.js',['javascript']);
+    gulp.watch( src + 'images/**/*',['images']);
+    
+    gulp.start('styles', 'javascript', 'connect');
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# boilerplate-frontend
+
+After cloning, init your project with `./impulse init <project name>`
