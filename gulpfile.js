b58cfb873a31916ae74e5884fa921191c0d9c5b3
Lemagus <gabriel@adjust.be>
2019-07-22 13:44:35 +0200

    [Fix] fixed files for Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,70 @@
 const gulp = require('gulp');
-const concat = require('gulp-concat');
+const concat = require('gulp-concat')
 
 const sass = require('gulp-sass'),
-	  sourcemaps = require('gulp-sourcemaps')
-	  postcss      = require('gulp-postcss');
+	  sourcemaps = require('gulp-sourcemaps'
+	  postcss      = require('gulp-postcss')
 
-const babel = require('gulp-babel');
+const babel = require('gulp-babel')
 
-const minifyJS = require('gulp-minify');
+const minifyJS = require('gulp-minify')
 
-const uglify = require('gulp-uglify');
-const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify')
+const eslint = require('gulp-eslint')
 
-const imagemin = require('gulp-imagemin');
-const connect = require('gulp-connect-php');
+const imagemin = require('gulp-imagemin')
+const connect = require('gulp-connect-php')
 
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 
-const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create()
 
-const src  = "./src/";
-const dist = "./dist/";
+const src  = "./src/"
+const dist = "./dist/"
 
-gulp.task('connect', function() {
+gulp.task('connect', (done) => {
+  console.log(`Invoking connect-php on PID: ${process.pid}.`)
+  
   connect.server({
     port: 3002,
-  }, function(){
+  }, () =>{
   		browserSync.init({
 			proxy: '127.0.0.1:3002',
 			open: false,
-			ghostMode: false
-		});
+			ghostMode: false,
+			files: [
+    			dist + 'css/*.css',
+    			dist + 'js/**/*.js',
+    			src + '../**/*.html',
+    			src + '../**/*.php'
+			]
+		})
+  })
+  
+  process.once('SIGUSR2', () => {
+    console.log("Got SIGUSR2, invoking callback.")
+    connect.closeServer()
+    done()
   });
+  
+  done()
 });
 
-gulp.task('styles', function() {
-    gulp.src( src + 'sass/**/*.scss')
+gulp.task('styles', () => {
+    return gulp.src( src + 'sass/**/*.scss')
     	.pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss([
-        	autoprefixer({ browsers: ['last 2 versions', 'ie >= 9', 'ChromeAndroid >= 2.3'] }),
+        	autoprefixer({ overrideBrowserslist: ['last 2 versions', 'ie >= 9'] }),
         	cssnano()
         ]))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest( dist + 'css/'));
+        .pipe(gulp.dest( dist + 'css/'))
 });
 
-gulp.task('javascript', function() {
-  	gulp.src([ src + 'js/**/*.js'])
+gulp.task('javascript', () => {
+  	return gulp.src([ src + 'js/**/*.js'])
   		.pipe(eslint())
   		.pipe(sourcemaps.init())
   		.pipe(eslint.format())
@@ -58,7 +74,8 @@ gulp.task('javascript', function() {
   		.pipe(concat('main.js'))
   		.pipe(uglify())
   		.pipe(sourcemaps.write('./'))
-  		.pipe(gulp.dest( dist + 'js/'));
+  		.pipe(gulp.dest( dist + 'js/'))
+  		
 });
 
 gulp.task('images', function () {
@@ -71,26 +88,18 @@ gulp.task('images', function () {
              progressive: true,
              svgoPlugins: [{removeViewBox: false}]
          }))
-         .pipe(gulp.dest(dist + 'images/'));
+         .pipe(gulp.dest(dist + 'images/'))
 });
 
 //Watch task
-gulp.task('default', ['styles', 'javascript', 'connect'], function() {
+gulp.task('default', () => {
 	
-    gulp.watch( src + 'sass/**/*.scss',['styles']);
-    gulp.watch( src + 'js/**/*.js',['javascript']);
-    
-    gulp.watch( dist + 'css/*.css', function(){
-	    gulp.src(dist + 'css/*.css')
-	   		.pipe(browserSync.stream());
-   }); 
-   
-    gulp.watch( src + '../**/*.php').on('change', browserSync.reload);
-    gulp.watch( src + '../**/*.html').on('change', browserSync.reload);
-    
-	gulp.watch( src + 'js/**/*.js').on('change', browserSync.reload);
+    gulp.watch( src + 'sass/**/*.scss', gulp.series('styles'))
+    gulp.watch( src + 'js/**/*.js', gulp.series('javascript'))
+	
+	gulp.parallel(['styles', 'javascript', 'connect'])()
 });
 
-gulp.task('compress', function(){
-	gulp.start('images');
+gulp.task('compress', () =>{
+	gulp.series(['images'])()
 });
